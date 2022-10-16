5f7097e31a9350822fb9f897b74b096656c310fe
vyas-git <vyasreddy.tech@gmail.com>
2022-10-16 23:11:16 +0530

    Longest palindrome string

--- palindrome/longest_substr.go
+++ palindrome/longest_substr.go
@@ -1,41 +1,91 @@
 package main
 
+import "fmt"
+
+func main() {
+	// test
+	input1 := "babad"
+	input2 := "cbbd"
+
+	output1 := longestPalindrome(input1)
+	output2 := longestPalindrome(input2)
+
+	expected1 := []string{"bab", "aba"}
+	expected2 := "bb"
+
+	if expected1[0] == output1 || expected1[1] == output1 {
+		fmt.Println("Success input 1 ", output1)
+	} else {
+		fmt.Println("Failed input 1 ", output1)
+	}
+
+	if expected2 == output2 {
+		fmt.Println("Success input 2 ", output2)
+	} else {
+		fmt.Println("Failed input 2 ", output2)
+	}
+
+}
+
+// Given a string s, return the longest palindromic substring in s.
+
+// A string is called a palindrome string if the reverse of that string is the same as the original string.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
 func longestPalindrome(s string) string {
+
 	if len(s) == 0 || len(s) == 1 {
 		return s
 	}
-	lStart, maxLen := 0, 1
-	for i := 0; i < len(s); {
-		if len(s)-i <= maxLen/2 {
-			break
+	longest_pl := ""
+	// iterate each character in string
+	for i := 0; i < len(s); i++ {
+
+		// from every character left and right charcter will be same
+		// So expand through from centre left and right
+
+		case1_pl := checkFromCentre(i-1, i+1, s)
+
+		// Some cases may have duplciate charcters adjacent
+		// so start left adjacent to right
+		case2_pl := checkFromCentre(i, i+1, s)
+
+		if len(case1_pl) > len(longest_pl) {
+			longest_pl = case1_pl
 		}
-		// skip duplicates
-		k, j := i, i
-		for k < len(s)-1 && s[k] == s[k+1] {
-			k++
+
+		if len(case2_pl) > len(longest_pl) {
+			longest_pl = case2_pl
 		}
-		// expand around centre
-		i = k + 1
-		for k < len(s)-1 && j > 0 && s[k+1] == s[j-1] {
-			k++
-			j--
-		}
-		newLen := k - j + 1
-		if newLen > maxLen {
-			maxLen = newLen
-			lStart = j
-		}
+
 	}
-	return s[lStart : lStart+maxLen]
-}
-func reverseStr(str string) string {
-	rns := []rune(str)
-	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
-		rns[i], rns[j] = rns[j], rns[i]
+	// if no palindrome then return first character of string
+	if longest_pl == "" {
+		return string(s[0])
 	}
-	return string(rns)
+	return longest_pl
 }
 
-func main() {
-	longestPalindrome("babad")
+func checkFromCentre(l int, r int, s string) string {
+	pl_str := ""
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		pl_str = s[l : r+1]
+		l--
+		r++
+	}
+	return pl_str
 }
